e10e608c5f4cf055e43eb64f025cea3fdcae69c7
inceon <mr.inceon@gmail.com>
2024-11-14 18:38:20 +0200

    again updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import {App} from './App.tsx';
 import {addAccessTokenInterceptor} from "./services/apiService.ts";
@@ -8,10 +8,10 @@ import 'react-toastify/dist/ReactToastify.css';
 
 addAccessTokenInterceptor();
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<HashRouter>
-			<App/>
-		</HashRouter>
-		<ToastContainer/>
-	</React.StrictMode>
+    <React.StrictMode>
+        <HashRouter basename="/">
+            <App/>
+        </HashRouter>
+        <ToastContainer/>
+    </React.StrictMode>
 );
