130086ce8f08c6afea0ab1d86c42908e57ec1dd5
inceon <mr.inceon@gmail.com>
2024-11-14 17:50:39 +0200

    unknown fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Login } from './pages/Login.page';
-import { useAuth } from './hooks/useAuth';
-import style from './App.module.css';
-import Home from './pages/Home/Home.page';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login.page";
+import { useAuth } from "./hooks/useAuth";
+import style from "./App.module.css";
+import Home from "./pages/Home/Home.page";
 
 export const App = () => {
-    const { isAuthenticated, isLoading } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
 
-    if (isLoading) {
-        return <div>Loading...</div>; // Or your loading component
-    }
+  if (isLoading) {
+    return <div>Loading...</div>; // Or your loading component
+  }
 
-    return (
-        <div className={style.mainContainer}>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        isAuthenticated ? (
-                            <Home />
-                        ) : (
-                            <Navigate to="/login" replace />
-                        )
-                    }
-                />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className={style.mainContainer}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Home />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            !isAuthenticated ? (
+              <Login />
+            ) : (
+              <Navigate to="/" replace />
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
